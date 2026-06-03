34300305b70502b0492a07674124e96bcf7486f1
Asim Bilal <asimbilal@mail.com>
2026-06-03 11:18:00 +0500

    wire up controller, DI container and app bootstrap

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,1 +1,40 @@
-// Fill here
+import { Request, Response } from 'express';
+import { inject } from 'inversify';
+import { controller, httpPost, request, response } from 'inversify-express-utils';
+import { BaseController, TYPES, validateRegisterDto, validateLoginDto, AppError } from '../lib';
+import { UserService } from '../services';
+
+@controller('/auth')
+export class UserController extends BaseController {
+    constructor(@inject(TYPES.UserService) private userService: UserService) {
+        super();
+    }
+
+    @httpPost('/register')
+    async register(@request() req: Request, @response() res: Response) {
+        try {
+            const dto = validateRegisterDto(req.body);
+            const result = await this.userService.register(dto);
+            return res.status(201).json(result);
+        } catch (err) {
+            if (err instanceof AppError) {
+                return res.status(err.statusCode).json({ error: err.message });
+            }
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+    }
+
+    @httpPost('/login')
+    async login(@request() req: Request, @response() res: Response) {
+        try {
+            const dto = validateLoginDto(req.body);
+            const result = await this.userService.login(dto);
+            return res.status(200).json(result);
+        } catch (err) {
+            if (err instanceof AppError) {
+                return res.status(err.statusCode).json({ error: err.message });
+            }
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,29 @@
 import { json } from 'body-parser';
-
 import 'reflect-metadata';
 import dotenv from 'dotenv';
 import { InversifyExpressServer } from 'inversify-express-utils';
 
-// import { createKafkaClient, Producer, Consumer } from '@marta/eventbus/dist';
-
-// import { getDataSource } from './typeormconfig';
-
+import { getDataSource } from './typeormconfig';
 import { diContainer } from '../inversify.config';
-// import { TYPES } from './lib';
-// import { exampleEventHandler } from './events/handlers';
+import { TYPES } from './lib';
 
 dotenv.config();
 
 (async () => {
     try {
-        // Create Kafka producer and consumer instance
-        // const kafkaClient = await createKafkaClient();
-        // const producer = new Producer(kafkaClient);
-        // const consumer = new Consumer(kafkaClient, 'test-service-group');
+        const dataSource = await getDataSource();
+        await dataSource.initialize();
+        diContainer.bind(TYPES.DB).toConstantValue(dataSource);
 
-        // Subscribe to all the topics the service is interested in
-        // await consumer.subscribe([
-        //     { topic: 'test-topic', eventHandler: exampleEventHandler },
-        // ]);
-
-        // Bind producer instance to the DI container so it can be accessed from anywhere
-        // diContainer.bind(TYPES.producer).toConstantValue(producer);
-
-        // DB setup
-        // const dataSource = await getDataSource();
-        // await dataSource.initialize();
-        // diContainer.bind(TYPES.DB).toConstantValue(dataSource);
-
-        // Create app server
         const app = new InversifyExpressServer(diContainer, null, {
             rootPath: '/partner-app/api',
         });
+
         app.setConfig(app => {
             app.use(json());
         });
 
         const server = app.build();
-
         const PORT = process.env.PORT || 9000;
 
         server.listen(PORT, () => {
@@ -51,5 +31,6 @@ dotenv.config();
         });
     } catch (err) {
         console.error(err);
+        process.exit(1);
     }
 })();

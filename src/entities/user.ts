96fc6756e5f784d4c949ec686dae543afcc80968
Asim Bilal <asimbilal@mail.com>
2026-06-03 09:48:00 +0500

    add user entity and repository

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,7 +1,25 @@
-// /* eslint-disable indent */
-// import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
-// @Entity({ name: 'users' })
-// export class User {
-// 
-// }
+@Entity({ name: 'users' })
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -1,1 +1,26 @@
-// Fill here
+import { injectable, inject } from 'inversify';
+import { DataSource, Repository } from 'typeorm';
+import { User } from '../entities';
+import { TYPES } from '../lib';
+
+export interface UserRepository {
+    findByEmail(email: string): Promise<User | null>;
+    save(user: Partial<User>): Promise<User>;
+}
+
+@injectable()
+export class UserRepositoryImpl implements UserRepository {
+    private repo: Repository<User>;
+
+    constructor(@inject(TYPES.DB) private db: DataSource) {
+        this.repo = db.getRepository(User);
+    }
+
+    findByEmail(email: string): Promise<User | null> {
+        return this.repo.findOne({ where: { email } });
+    }
+
+    save(user: Partial<User>): Promise<User> {
+        return this.repo.save(user);
+    }
+}
